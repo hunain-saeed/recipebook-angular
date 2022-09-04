216439022dc8b970ecad6b5164cb686bea1cc590
hunains <hunains@vinncorp.com>
2022-09-04 10:51:16 +0500

    some error occur while compiling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent
   ],
   imports: [
     BrowserModule
