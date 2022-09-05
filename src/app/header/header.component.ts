bfa9ef053b14f08f0a1c4d547a602cec79c269e8
hunains <hunains@vinncorp.com>
2022-09-05 09:37:21 +0500

    6. CP - Components & Databinding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
-
-@Component(
-    {
-        selector: 'app-header',
-        templateUrl: './header.component.html'
-    }
-)
-export class HeaderComponent{
-
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+  @Output() featureSeleted = new EventEmitter<string>();
+  onSelect(feature: string) {
+    this.featureSeleted.emit(feature);
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,26 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
-      'A Test',
+      'A Test 1',
+      'this is simple test',
+      'https://image.shutterstock.com/image-photo/unrecognizable-woman-looking-recipes-cookbook-600w-1959865639.jpg'
+    ),
+    new Recipe(
+      'A Test 2',
       'this is simple test',
       'https://image.shutterstock.com/image-photo/unrecognizable-woman-looking-recipes-cookbook-600w-1959865639.jpg'
     ),
   ];
 
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
